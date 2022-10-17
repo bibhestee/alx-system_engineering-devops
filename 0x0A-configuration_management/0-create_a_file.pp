defc4001e28bcabac177894cf63b421026cbbc38
Bibest <bibhestee@gmail.com>
2022-10-17 13:17:53 +0000

    Create a file in /tmp using puppet manifest

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# Create a file in the /tmp dir with some specified requirements
+file {'/tmp/':
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
