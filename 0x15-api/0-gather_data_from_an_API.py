f8808a7dbf56d55313349e2ab465f8886f208173
Bibest <bibhestee@gmail.com>
2022-11-14 23:13:31 +0100

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,24 +7,28 @@
     about his/her TODO list progress.
 """
 
-import requests
-import sys
 
-id = sys.argv[1]
+def api_data():
+    """a Python script that, using this REST API,
+    for a given employee ID, returns information
+    about his/her TODO list progress."""
 
-url = "https://jsonplaceholder.typicode.com"\
-      "/users/{}/todos/".format(id)
-url_2 = "https://jsonplaceholder.typicode.com"\
-        "/users/{}".format(id)
+    import requests
+    import sys
 
-response = requests.get(url)
-response_2 = requests.get(url_2)
+    id = sys.argv[1]
 
-tasks = response.json()
-details = response_2.json()
+    url = "https://jsonplaceholder.typicode.com"\
+          "/users/{}/todos/".format(id)
+    url_2 = "https://jsonplaceholder.typicode.com"\
+            "/users/{}".format(id)
 
-if __name__ == "__main__":
-    # Main
+    response = requests.get(url)
+    response_2 = requests.get(url_2)
+
+    tasks = response.json()
+    details = response_2.json()
+
     user = details.get('name')
     total_task = len(tasks)
     done_task = 0
@@ -38,3 +42,7 @@ if __name__ == "__main__":
     print(output)
     for title in titles:
         print('\t ' + title)
+
+
+if __name__ == "__main__":
+    api_data()
